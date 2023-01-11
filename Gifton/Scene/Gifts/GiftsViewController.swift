d8eaad33679a65172d2889ad222de08422c50c5d
Yang <38071744+crea9813@users.noreply.github.com>
2023-01-11 10:27:56 +0900

    ✨ Move Grid View into ScrollView

--- Gifton/Scene/Gifts/GiftsViewController.swift
+++ Gifton/Scene/Gifts/GiftsViewController.swift
@@ -24,6 +24,18 @@ final class GiftsViewController: UIViewController {
     
     private let searchBar = SearchField()
     
+    private let filterButton = UIButton()
+        .then {
+            $0.setImage(UIImage(named: "drawer"), for: .normal)
+            $0.backgroundColor = UIColor(hex: "E9E9EC")
+            $0.layer.cornerRadius = 15
+        }
+    
+    private let scrollView = UIScrollView()
+        .then {
+            $0.showsVerticalScrollIndicator = false
+        }
+    
     private let categoriesCollectionView: UICollectionView = {
         let flowLayout = UICollectionViewFlowLayout()
         flowLayout.estimatedItemSize = CGSize(width: 70, height: 120)
@@ -52,7 +64,7 @@ final class GiftsViewController: UIViewController {
         flowLayout.minimumLineSpacing = 12
         
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
-        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.isScrollEnabled = false
         collectionView.register(GiftCollectionViewCell.self, forCellWithReuseIdentifier: GiftCollectionViewCell.reuseIdentifier)
         collectionView.contentInset = UIEdgeInsets(top: 0, left: 25, bottom: 0, right: 25)
         
@@ -95,41 +107,73 @@ final class GiftsViewController: UIViewController {
         
         output
             .gifts
-            .do(onNext: {
-                print($0)
-            })
             .drive(giftCollectionView.rx.items(cellIdentifier: GiftCollectionViewCell.reuseIdentifier, cellType: GiftCollectionViewCell.self)) {
                 index, viewModel, cell in
                 cell.bind(with: viewModel)
             }.disposed(by: disposeBag)
+        
+        output
+            .gifts
+            .map { $0.isEmpty }
+            .drive(onNext: {
+                [weak self] isEmpty in
+                guard let self = self else { return }
+                if isEmpty {
+                    
+                } else {
+                    let height = Float(262) * ceil(Float(self.giftCollectionView.numberOfItems(inSection: 0) / 2))
+                    print(self.giftCollectionView.numberOfItems(inSection: 0))
+                    self.giftCollectionView.snp.updateConstraints {
+                        $0.height.equalTo(height)
+                    }
+                }
+            })
+            .disposed(by: disposeBag)
     }
     
     private func setupUI() {
         
+        self.navigationController?.isNavigationBarHidden = true
+        
         self.view.addSubview(searchBar)
-        self.view.addSubview(categoriesCollectionView)
-        self.view.addSubview(tableTitleLabel)
-        self.view.addSubview(giftCollectionView)
+        self.view.addSubview(filterButton)
+        self.view.addSubview(scrollView)
+        scrollView.addSubview(categoriesCollectionView)
+        scrollView.addSubview(tableTitleLabel)
+        scrollView.addSubview(giftCollectionView)
         
         self.searchBar.snp.makeConstraints {
             $0.top.equalTo(self.view.safeAreaLayoutGuide).offset(8)
-            $0.leading.trailing.equalToSuperview().inset(30)
+            $0.leading.equalToSuperview().inset(25)
+        }
+        
+        self.filterButton.snp.makeConstraints {
+            $0.top.bottom.equalTo(self.searchBar)
+            $0.width.equalTo(self.filterButton.snp.height)
+            $0.leading.equalTo(self.searchBar.snp.trailing).offset(12)
+            $0.trailing.equalToSuperview().inset(25)
+        }
+        
+        self.scrollView.snp.makeConstraints {
+            $0.top.equalTo(self.searchBar.snp.bottom).offset(30)
+            $0.leading.trailing.bottom.equalToSuperview()
         }
         
         self.categoriesCollectionView.snp.makeConstraints {
-            $0.top.equalTo(searchBar.snp.bottom).offset(30)
+            $0.top.equalToSuperview()
             $0.height.equalTo(120)
             $0.leading.trailing.equalToSuperview()
         }
         
         self.tableTitleLabel.snp.makeConstraints {
             $0.top.equalTo(categoriesCollectionView.snp.bottom).offset(25)
-            $0.leading.equalToSuperview().inset(30)
+            $0.leading.equalToSuperview().inset(25)
         }
         
         self.giftCollectionView.snp.makeConstraints {
             $0.top.equalTo(tableTitleLabel.snp.bottom).offset(10)
-            $0.leading.trailing.bottom.equalToSuperview()
+            $0.width.leading.trailing.bottom.equalToSuperview()
+            $0.height.equalTo(300)
         }
     }
 }
